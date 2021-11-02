3e0a4ef42346445ceb2c4494742b3e5ebd7b9419
BillyBernard <billybernard54@gmail.com>
2021-11-02 11:01:18 -0600

    worked on seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,35 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'tundra',
+        mileage: 25000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        model: 'forunner',
+        mileage: 250000,
+        title: 'salvage',
+    },
+    {
+        vin: '1111111111111',
+        make: 'ford',
+        model: 'focus',
+        mileage: 10000,
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars').truncate().then(() => {
+//         return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
