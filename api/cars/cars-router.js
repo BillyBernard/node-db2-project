06c0af9c5e3088861fb49c366dfd38d82de1ffd1
BillyBernard <billybernard54@gmail.com>
2021-11-02 12:22:16 -0600

    worked on middleware

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,14 +1,26 @@
 // DO YOUR MAGIC
 const express = require('express')
+const Car = require('./cars-model')
+const { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique } = require('./cars-middleware')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-    res.json('getting all cars')
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
 })
 
-router.get('/:id', async (req, res, next) => {
-    res.json(`getting car with id ${req.params.id}`)
+router.get('/:id', checkCarId, async (req, res, next) => {
+    try {
+        const car = await Car.getById(req.params.id)
+        res.json(car)
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.post('/', async (req, res, next) => {
--- api/server.js
+++ api/server.js
@@ -2,8 +2,6 @@ const express = require("express")
 const carsRouter = require('./cars/cars-router')
 const server = express()
 
-
-
 // DO YOUR MAGIC
 server.use(express.json())
 
